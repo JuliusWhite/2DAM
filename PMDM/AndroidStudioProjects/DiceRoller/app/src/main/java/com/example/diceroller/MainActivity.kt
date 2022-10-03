054cb2211ad97ee0c71ffa94b5529df549895f4d
JuliusWhite <julianlagogonzalez@gmail.com>
2022-10-03 19:56:41 +0200

    Almost done

--- PMDM/AndroidStudioProjects/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ PMDM/AndroidStudioProjects/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,8 +11,8 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var roll_text : TextView
-    private lateinit var resultImages : ImageView
+    private lateinit var roll_text: TextView
+    private lateinit var resultImages: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +22,16 @@ class MainActivity : AppCompatActivity() {
         resultImages = findViewById(R.id.dice_img)
 
         resultImages.setOnClickListener {
-            val animation : Animation = AnimationUtils.
-                loadAnimation(applicationContext, R.anim.animation)
+            val animation: Animation =
+                AnimationUtils.loadAnimation(applicationContext, R.anim.animation)
             animation.start()
             rollDice()
         }
-
-
     }
 
     private fun rollDice() {
-        val randomInt = (1 .. 6).random()
-        val drawableImage = when(randomInt){
+        val randomInt = (1..6).random()
+        val drawableImage = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -44,6 +42,6 @@ class MainActivity : AppCompatActivity() {
         val tvText = randomInt.toString()
         resultImages.setImageResource(drawableImage)
         roll_text.text = "Dice Roll is $tvText"
-        Toast.makeText(this, "Dice Roll is $tvText",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Dice Roll is $tvText", Toast.LENGTH_SHORT).show()
     }
 }
