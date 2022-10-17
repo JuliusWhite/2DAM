705c0447c018efd0dca0387d95c479679f28f624
JuliusWhite <julianlagogonzalez@gmail.com>
2022-10-17 23:13:45 +0200

    Added toast

--- PMDM/AndroidStudioProjects/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ PMDM/AndroidStudioProjects/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,11 +7,12 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var roll_text : TextView
-    private lateinit var resultImage : ImageView
+    private lateinit var roll_text: TextView
+    private lateinit var resultImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +21,8 @@ class MainActivity : AppCompatActivity() {
         roll_text = findViewById(R.id.roll_text)
         resultImage = findViewById(R.id.dice_img)
 
-        resultImage.setOnClickListener{
-            val animation : Animation = AnimationUtils
+        resultImage.setOnClickListener {
+            val animation: Animation = AnimationUtils
                 .loadAnimation(applicationContext, R.anim.animation)
             animation.start()
 
@@ -36,16 +37,21 @@ class MainActivity : AppCompatActivity() {
         val randomInt = (1..6).random()
 
         val drawableImage = when (randomInt) {
-        1-> R. drawable. dice_1
-        2-> R. drawable. dice_2
-        3-> R. drawable. dice_3
-        4-> R. drawable. dice_4
-        5-> R.drawable. dice_5
-        else ->
-        R. drawable. dice_6
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else ->
+                R.drawable.dice_6
+        }
+
+        val tvText = randomInt.toString()
+
+        resultImage.setImageResource (drawableImage)
+        roll_text.text = "Dice Roll OS $tvText"
+        Toast.makeText(this, "Dice Roll OS $tvText", Toast.LENGTH_SHORT).show()
+
+
     }
-
-
-
-}
 }
