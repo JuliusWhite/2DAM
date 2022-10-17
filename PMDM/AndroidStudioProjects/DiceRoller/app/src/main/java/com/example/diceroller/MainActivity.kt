842811670049ac95aff95d6935f1e08f9986604a
JuliusWhite <julianlagogonzalez@gmail.com>
2022-10-17 23:08:31 +0200

    Added random dice face

--- PMDM/AndroidStudioProjects/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ PMDM/AndroidStudioProjects/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -25,9 +25,27 @@ class MainActivity : AppCompatActivity() {
                 .loadAnimation(applicationContext, R.anim.animation)
             animation.start()
 
-            
+            rollDice()
 
         }
 
     }
+
+    private fun rollDice() {
+
+        val randomInt = (1..6).random()
+
+        val drawableImage = when (randomInt) {
+        1-> R. drawable. dice_1
+        2-> R. drawable. dice_2
+        3-> R. drawable. dice_3
+        4-> R. drawable. dice_4
+        5-> R.drawable. dice_5
+        else ->
+        R. drawable. dice_6
+    }
+
+
+
+}
 }
