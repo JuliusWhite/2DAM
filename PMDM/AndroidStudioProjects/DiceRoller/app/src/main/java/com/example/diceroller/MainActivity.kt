f75ed82dcce4eedfa9df488a5daaeff87d60d46a
JuliusWhite <julianlagogonzalez@gmail.com>
2022-10-17 23:00:46 +0200

    Added animation to main activity

--- PMDM/AndroidStudioProjects/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ PMDM/AndroidStudioProjects/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.diceroller
 
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var roll_text : TextView
+    private lateinit var resultImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        roll_text = findViewById(R.id.roll_text)
+        resultImage = findViewById(R.id.dice_img)
+
+        resultImage.setOnClickListener{
+            val animation : Animation = AnimationUtils
+                .loadAnimation(applicationContext, R.anim.animation)
+            animation.start()
+
+            
+
+        }
+
     }
 }
