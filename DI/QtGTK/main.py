60e140d16b2866bd38657f7ee8043c5c76eac695
JuliusWhite <julianlagogonzalez@gmail.com>
2022-10-19 14:47:23 +0200

    Added PyQt

--- DI/QtGTK/main.py
+++ DI/QtGTK/main.py
@@ -1,13 +1,15 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-class MainWindow (QMainWindow):
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("My first window with PyQt6")
 
         self.show()
+
 
 aplication = QApplication(sys.argv)
 window = MainWindow()
