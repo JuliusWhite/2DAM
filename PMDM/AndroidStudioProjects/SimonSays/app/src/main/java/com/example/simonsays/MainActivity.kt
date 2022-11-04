e0ba83dc564bc945db805b779456467983f0ecc2
JuliusWhite <julianlagogonzalez@gmail.com>
2022-11-04 18:30:47 +0100

    Added onclic

--- PMDM/AndroidStudioProjects/SimonSays/app/src/main/java/com/example/simonsays/MainActivity.kt
+++ PMDM/AndroidStudioProjects/SimonSays/app/src/main/java/com/example/simonsays/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.simonsays
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultImages: ImageView
+    private lateinit var resultText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultImages = findViewById(R.id.white_circle)
+        resultText = findViewById(R.id.greeting_txt)
+        resultImages.setOnClickListener {
+            resultText.setText(getString(R.string.score))
+            resultText.setTextSize(32F)
+            resultText.setTextColor((getResources().getColor(R.color.black)))
+        }
     }
 }
+
