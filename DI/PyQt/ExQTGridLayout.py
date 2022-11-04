b61175a7648c9c79a96e3daace317d26c5e34eb2
JuliusWhite <julianlagogonzalez@gmail.com>
2022-11-04 14:42:25 +0100

    Added operators

--- DI/PyQt/ExQTGridLayout.py
+++ DI/PyQt/ExQTGridLayout.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6.QtWidgets import (QApplication, QWidget,
-                             QPushButton, QGridLayout, QMainWindow, QVBoxLayout)
+                             QPushButton, QGridLayout, QMainWindow, QVBoxLayout, QLineEdit, QHBoxLayout)
+
 
 class Example(QMainWindow):
 
@@ -20,7 +21,7 @@ class Example(QMainWindow):
         grid.addWidget(QPushButton('7'), 2, 0)
         grid.addWidget(QPushButton('8'), 2, 1)
         grid.addWidget(QPushButton('9'), 2, 2)
-        grid.setContentsMargins(0, 0, 0, 6)
+        grid.setContentsMargins(0, 6, 0, 6)
 
         gridContainer = QWidget()
         gridContainer.setLayout(grid)
@@ -28,16 +29,34 @@ class Example(QMainWindow):
 
         boxV = QVBoxLayout()
         boxV.setSpacing(0)
+        boxV.addWidget(QLineEdit())
         boxV.addWidget(gridContainer)
         boxV.addWidget(QPushButton('0'))
 
+        operatorKeyboard = QGridLayout()
+        operatorKeyboard.addWidget(QPushButton('+'), 0, 0)
+        operatorKeyboard.addWidget(QPushButton('-'), 1, 0)
+        operatorKeyboard.addWidget(QPushButton('x'), 2, 0)
+        operatorKeyboard.addWidget(QPushButton('%'), 3, 0)
+        operatorKeyboard.addWidget(QPushButton('='), 4, 0)
+        operatorKeyboard.setContentsMargins(0, 6, 0, 6)
+
+        boxH = QHBoxLayout()
+        control = QWidget()
+        control.setLayout(boxV)
+        boxH.addWidget(control)
+        control = QWidget()
+        control.setLayout(operatorKeyboard)
+        boxH.addWidget(control)
+
         container = QWidget()
-        container.setLayout(boxV)
+        container.setLayout(boxH)
         self.setCentralWidget(container)
 
         self.show()
 
+
 if __name__ == '__main__':
-     app = QApplication(sys.argv)
-     ex = Example()
-     sys.exit(app.exec())
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
