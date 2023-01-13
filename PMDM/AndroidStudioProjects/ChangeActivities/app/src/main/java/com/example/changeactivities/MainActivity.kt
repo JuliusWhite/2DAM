2453ff6fbba5a8171d1c01bc5b87e2e94b2e0b8b
JuliusWhite <julianlagogonzalez@gmail.com>
2023-01-13 17:24:10 +0100

    Added onActivityResult

--- PMDM/AndroidStudioProjects/ChangeActivities/app/src/main/java/com/example/changeactivities/MainActivity.kt
+++ PMDM/AndroidStudioProjects/ChangeActivities/app/src/main/java/com/example/changeactivities/MainActivity.kt
@@ -1,21 +1,53 @@
 package com.example.changeactivities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
+
+// definition of requestCode
+val RESULT_ONE = 1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var chgBtn = findViewById<Button>(R.id.changeBtn1)
 
+
         chgBtn.setOnClickListener {
+            // instantiation of the Intent to go to the second activity
             val intent = Intent(this, MainActivity2::class.java)
+
+            // data to senfd to the second activity
             intent.putExtra("clients", 5)
             intent.putExtra("providers", 50)
-            startActivity(intent)
+
+
+            startActivityForResult(intent, RESULT_ONE)
+        }
+    }
+
+    // funccion called when the second activity sets a result
+    @Deprecated("Deprecated a partir de API 30")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val greeting = findViewById<TextView>(R.id.textViewResult)
+
+        // checks correct result
+        if (resultCode != Activity.RESULT_OK) return
+        when(requestCode) {
+            RESULT_ONE -> {
+                // if Intent is not null shoew greeting message
+                if (data != null) {
+                    greeting.text = data.getStringExtra("greeting")
+                }
+                else {}
+            }
         }
     }
 }
--- PMDM/AndroidStudioProjects/ChangeActivities/app/src/main/java/com/example/changeactivities/MainActivity2.kt
+++ PMDM/AndroidStudioProjects/ChangeActivities/app/src/main/java/com/example/changeactivities/MainActivity2.kt
@@ -1,5 +1,6 @@
 package com.example.changeactivities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,20 +11,29 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        var chgBtn2 = findViewById<Button>(R.id.changeBtn2)
+//        var chgBtn2 = findViewById<Button>(R.id.changeBtn2)
 
-        chgBtn2.setOnClickListener {
-            val intent2 = Intent(this, MainActivity::class.java)
-            startActivity(intent2)
-        }
+//        chgBtn2.setOnClickListener {
+//            val intent2 = Intent(this, MainActivity::class.java)
+//            startActivity(intent2)
+//        }
 
-        val intent = intent
+        // geting the previous Intent
+        val intent = getIntent()
 
+        // getting the intent's values
         val clientsValue = intent.getIntExtra("clients", 0)
         val providersValue = intent.getIntExtra("providers", 0)
         val numClients = findViewById<TextView>(R.id.txtViewClients)
         val numProviders = findViewById<TextView>(R.id.txtViewProviders)
         numClients.text = "Clientes: $clientsValue"
         numProviders.text = "Providers: $providersValue"
+
+        // send new data to the previous activity
+        intent.putExtra("greeting", "Greetings!");
+        setResult(Activity.RESULT_OK, intent);
+
+        // close the activity
+        finish()
     }
 }
