ed99f3ebd90a584eb3f163c98e538e1f1a3767f9
JuliusWhite <julianlagogonzalez@gmail.com>
2023-01-25 18:50:31 +0100

    Some changes to some files

--- DI/Reportlab/Draw.py
+++ DI/Reportlab/Draw.py
@@ -1,10 +1,14 @@
+from sys import platform
 from reportlab.graphics.shapes import Image, Drawing
 from reportlab.graphics import renderPDF
 from reportlab.lib.pagesizes import A4
 
 op = []
 
-img = Image(250, 0, 400, 400, "/home/dam2a/Imaxes/Wallpapers/comunist_pingu.jpg")
+if platform == "linux" or platform == "linux2":
+    img = Image(250, 0, 400, 400, "/home/dam2a/Imaxes/Wallpapers/comunist_pingu.jpg")
+elif platform == "win32":
+    img = Image(200, 0, 400, 300, "C:\\Users\\julia\\Downloads\\king_bugs_bunny.jpg")
 
 draw = Drawing(30, 30)
 draw.add(img)
