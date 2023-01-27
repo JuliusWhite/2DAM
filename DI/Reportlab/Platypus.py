d9bbd78a46fa8373f4b50013baf976fc19267548
JuliusWhite <julianlagogonzalez@gmail.com>
2023-01-27 14:46:10 +0100

    Added Tables.py and working in Platypus.py

--- DI/Reportlab/Platypus.py
+++ DI/Reportlab/Platypus.py
@@ -1,11 +1,23 @@
+import os.path
+from sys import platform
+
 from reportlab.platypus import Paragraph
 from reportlab.platypus import Image
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Spacer
 
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
+
+
+class MyParagraphStyle(ParagraphStyle):
+    def __init__(self):
+        super().__init__(self, fontSize=14,
+                         font='Helvetica',
+                         textColor='Red',
+                         leftIndent=105)
+
 
 style_sheet = getSampleStyleSheet()
 
@@ -19,6 +31,24 @@ header.backColor = colors.paleturquoise
 
 paragarph = Paragraph("Docs header", header)
 op.append(paragarph)
+
+string_ = """Paragraph content text to repeat. """
+regular_text = style_sheet['BodyText']
+paragraph2 = Paragraph(string_ * 100, regular_text)
+op.append(paragraph2)
+
+op.append(Spacer(0, 20))
+
+img_path = ""
+if platform == "linux" or platform == "linux2":
+    img_path = "/home/dam2a/Imaxes/Wallpapers/comunist_pingu.jpg"
+elif platform == "win32":
+    img_path = "C:\\Users\\julia\\Downloads\\king_bugs_bunny.jpg"
+img = Image(os.path.realpath(img_path), width=225, height=225)
+op.append(img)
+
+paragraph3 = Paragraph(string_ * 5, MyParagraphStyle())
+op.append(paragraph3)
 
 doc = SimpleDocTemplate("Platypus.pdf", pagesize=A4, showBoundary=1)
 doc.build(op)
---
+++ DI/Reportlab/Tables.py
@@ -0,0 +1,21 @@
+from reportlab.platypus import Paragraph, Image, SimpleDocTemplate, \
+    Spacer, Table, TableStyle, PageBreak
+
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+
+doc = SimpleDocTemplate("Tables.pdf", pagesize=A4)
+
+op = []
+
+data = [('', 'Sales', 'Bougths'),
+        ('January', 300, 500),
+        ('February', 400, 500),
+        ('March', 30, 200)]
+
+table = Table(data, colWidths=100, rowHeights=30)
+table.setStyle([])
+
+op.append(table)
+doc.build(op)
