51bc81f960fbb84403b049ff568a1f5a15a3deb0
JuliusWhite <julianlagogonzalez@gmail.com>
2023-03-02 12:03:19 +0100

    Added AD/Drivers and Ended JavaPostgresMongo proyect

--- AD/MongoPoostgresJava/src/Main.java
+++ AD/MongoPoostgresJava/src/Main.java
@@ -1,9 +1,11 @@
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import java.sql.*;
+import java.util.Iterator;
 
 public class Main {
 
@@ -26,14 +28,58 @@ public class Main {
         }
     }
 
-    public static void confitmation(double id) {
+    public static void confitmation(double id, MongoCollection<Document> collection) {
+        Document filter = new Document("_id", 1);
+        Document update = new Document("$set", new Document("confirmado", 1));
+        collection.updateOne(filter, update);
+        System.out.println("\nSeted filght 1 as confirmado");
+
+        // Getting the iterable object
+        FindIterable<Document> iterDoc = collection.find();
+
+        // Getting the iterator
+        Iterator<Document> it = iterDoc.iterator();
+        iterDoc = collection.find(filter);
+
+        // Getting outward flight id
+        it = iterDoc.iterator();
+        double outwardFlight = (double) it.next().get("idvooida");
+        System.out.println("Outward flight: " + outwardFlight);
+
+        // Getting arrival flight id
+        it = iterDoc.iterator(); // here im restarting the iterator
+        double arrivalFlight = (double) it.next().get("idvoovolta");
+        System.out.println("Arrival flight: " + arrivalFlight);
+
+        // Insatiation of the variables that are going to be used to save postgres data
+        int outwardFlightPrize = 0;
+        int arrivalFlightPrize = 0;
+        int total = 0;
+
+        try {
+            // Starting the postgres connection from the class DBConnection
+            DBConnection.conexion();
+
+            PreparedStatement ps = DBConnection.conexion().prepareStatement("SELECT * FROM voos");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                // getting prices from voos postgres
+                if (rs.getInt("voo") == outwardFlight) outwardFlightPrize = rs.getInt("prezo");
+                else if (rs.getInt("voo") == arrivalFlight) arrivalFlightPrize = rs.getInt("prezo");
+            }
+            total = outwardFlightPrize + arrivalFlightPrize;
+            System.out.println("prezo ida: " + outwardFlightPrize + ", prezo volta: " + arrivalFlightPrize + ", total: " + total);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        update = new Document("$set", new Document("prezoreserva", total));
+        collection.updateOne(filter, update);
+        System.out.println("\nSeted prezoreserva as " + total);
 
     }
 
-    public static void main(String[] args) throws SQLException {
-
-        // Starting the postgres connection from the class DBConnection
-        DBConnection.conexion();
+    public static void main(String[] args) {
 
         // Creating a Mongo client
         MongoClient mongo = new MongoClient("localhost", 27017);
@@ -46,7 +92,7 @@ public class Main {
         System.out.println("\nCollection myCollection selected successfully");
 
         flightReservation(1, "361a", 1, 2, 0, collection);
-
+        confitmation(1, collection);
     }
 
 }
