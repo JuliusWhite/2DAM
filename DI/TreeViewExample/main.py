ef7345fdcfb42b6a857c12258dd4846051fa5baf
JuliusWhite <julianlagogonzalez@gmail.com>
2023-03-06 14:50:06 +0100

    Adde treeview example

--- DI/TreeViewExample/main.py
+++ DI/TreeViewExample/main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import sqlite3
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Connect to the database and retrieve the data
+conn = sqlite3.connect('perfisUsuarios.bd')
+c = conn.cursor()
 
+c.execute('SELECT idPefil, nomePerfil, descricion FROM perfis')
+data1 = c.fetchall()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+c.execute('SELECT dniUsuario, idPerfil, permiso FROM perfisUsuario')
+data2 = c.fetchall()
 
+c.execute('SELECT nome, dni, departamento, correoe, activo FROM usuarios')
+data3 = c.fetchall()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Set the table style
+table_style = TableStyle([
+    ('BACKGROUND', (0, 0), (-1, 0), colors.gray),
+    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+    ('ALIGN', (0, 0), (-1, 0), 'CENTER'),
+    ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+    ('FONTSIZE', (0, 0), (-1, 0), 14),
+    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+    ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+    ('TEXTCOLOR', (0, 1), (-1, -1), colors.black),
+    ('ALIGN', (0, 1), (-1, -1), 'CENTER'),
+    ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),
+    ('FONTSIZE', (0, 1), (-1, -1), 12),
+    ('BOTTOMPADDING', (0, 1), (-1, -1), 8),
+])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+table1 = Table([('idPefil', 'nomePerfil', 'descricion')] + data1, style=table_style)
+table2 = Table([('dniUsuario', 'idPefil', 'permiso')] + data2, style=table_style)
+table3 = Table([('Nome', 'DNI', 'Departamento', 'Correoe', 'Activo')] + data3, style=table_style)
+
+op = []
+
+op.append(table1)
+op.append(table2)
+op.append(table3)
+# Create a PDF document and add a table with the data
+pdf_doc = SimpleDocTemplate('SQLite.pdf', pagesize=A4)
+
+# Add the table to the PDF document and save it
+pdf_doc.build(op)
+
+# Closing db connection
+conn.close()
