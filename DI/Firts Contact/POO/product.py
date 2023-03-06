4e84fb467a535415e48c5c883829180c26ef01e2
JuliusWhite <julianlagogonzalez@gmail.com>
2023-03-06 01:45:09 +0100

    Readded DI

--- DI/Firts Contact/POO/product.py
+++
@@ -1,8 +0,0 @@
-class Product:
-    def __init__(self, cod, stock, price):
-        self.cod = cod
-        self.stock = stock
-        self.price = price
-
-    def __str__(self) -> str:
-        return "Cod: " + str(self.cod) + "\nStock: " + str(self.stock) + "\nPrice: " + str(self.price)
